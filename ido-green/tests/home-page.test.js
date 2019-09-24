772f1e0b3ebf6d651d67d3757b357a89d6f8cd5a
Ido Green <greenido@gmail.com>
2019-09-24 16:38:12 -0700

    Update home-page.test.js

--- ido-green/tests/home-page.test.js
+++ ido-green/tests/home-page.test.js
@@ -2,7 +2,6 @@ describe('main-page', () => {
   beforeAll(async () => {
     await page.goto('https://ido-green.appspot.com');
   });
-
   it('should be titled "Ido Green - Software, Conferences and Projects"', async () => {
     await expect(page.title()).resolves.toMatch('Ido Green - Software, Conferences and Projects');
   });
